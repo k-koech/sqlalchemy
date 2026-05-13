f8ed30fac34106117dcec536c35d4e3e1aec30eb
kipchumba2024 <triplek@gmail.com>
2026-05-13 18:22:44 +0300

    done

--- server/app.py
+++ server/app.py
@@ -15,14 +15,29 @@ CORS(app)
 app.secret_key = "sehtrsdyhndtejdydunuyehbdrvteryhe"
 
 
-# model
+# MODELS
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+
+    posts = db.relationship("Post", backref='user')
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
 
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id") )
+    comments = db.relationship("Comment", backref='post')
 
-# CRUD
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.Text, nullable=False)
+
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id"))
+
+
+# ==================================== CRUD POST ================================================================
 # READ
 @app.route("/posts")
 def fetch_posts():
@@ -44,8 +59,72 @@ def fetch_posts():
 # UPDATE
 # DELETE
 
+# ADD
+@app.route("/posts", methods=["POST"])
+def add_post():
+    data = request.get_json()
 
+    new_post = Post(
+        title=data["title"],
+        content=data["content"]
+    )
+
+    db.session.add(new_post)
+    db.session.commit()
+
+    return jsonify({"success": "Post created successfully"}), 201
+
+
+# read one post
+@app.route("/posts/<int:post_id>")
+def fetch_post(post_id):
+    post = Post.query.get(post_id)
+    if not post:
+        return jsonify({"error": "Post does not exists"}), 404
+    
+    my_post = {
+        "id": post.id,
+        "title": post.title,
+        "content": post.content
+    }
+    return jsonify(my_post), 200
+
+
+# -0---UPDATE
+@app.route("/posts/<int:id>", methods=["PUT"])
+def update_post(id):
+    # fetch the post
+    post = Post.query.get(id)
+    # if post doesn't exists give an error
+    if not post:
+        return jsonify({"error": "Post does not exists"}), 404
+    
+
+    data = request.get_json()
+
+    post.title = data.get("title", post.title)
+    post.content = data.get("content", post.content)
+
+    db.session.commit()
+
+    return jsonify({"success": "Post updated successfully"}), 200
+
+
+# DELETE
+@app.route("/posts/<int:id>", methods=["DELETE"])
+def delete_post(id):
+    # fetch the post
+    post = Post.query.get(id)
+    # if post doesn't exists give an error
+    if not post:
+        return jsonify({"error": "The post you want to delete does not exists"}), 404
+
+    db.session.delete(post)
+    db.session.commit()
+
+    return jsonify({"success": "Post deleted successfully"}), 200
 
 # export FLASK_APP=app.py
 # export FLASK_DEBUG=1
 
+# ==================================== CRUD POST ================================================================
